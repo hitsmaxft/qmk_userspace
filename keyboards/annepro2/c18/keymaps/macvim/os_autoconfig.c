78dd1c3c96e9b8933766bc5c7fa61c4f1b85be15
histmaxft <mfthits@gmail.com>
2024-03-11 21:04:11 +0800

    修复了在Windows系统下Annpro2 c18键盘的自动配置问题，更新了keymap配置。

--- keyboards/annepro2/c18/keymaps/macvim/os_autoconfig.c
+++ keyboards/annepro2/c18/keymaps/macvim/os_autoconfig.c
@@ -5,11 +5,15 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
     if (!process_detected_host_os_user(detected_os)) {
         return false;
     }
-    
+
     keymap_config.raw = eeconfig_read_keymap();
 
     switch (detected_os) {
         case OS_WINDOWS:
+            if (!keymap_config.swap_lalt_lgui){
+                keymap_config.swap_lalt_lgui = true;
+                eeconfig_update_keymap(keymap_config.raw);
+            }
             keymap_config.swap_lalt_lgui = true;
             eeconfig_update_keymap(keymap_config.raw);
             break;
@@ -17,8 +21,10 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
         case OS_IOS:
         case OS_LINUX:
         case OS_UNSURE:
-            keymap_config.swap_lalt_lgui = false;
-            eeconfig_update_keymap(keymap_config.raw);
+            if (keymap_config.swap_lalt_lgui){
+                keymap_config.swap_lalt_lgui = false;
+                eeconfig_update_keymap(keymap_config.raw);
+            }
             break;
     }
 
