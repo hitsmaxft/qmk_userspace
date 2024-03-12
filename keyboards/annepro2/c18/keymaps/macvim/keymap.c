cf2358d494950ec58321b054e152751884834be6
hitsmaxft <mfthits@gmail.com>
2024-03-12 13:23:46 +0800

    为macvim键图添加reset_to_iap函数，以在启动时进入iap模式。

--- keyboards/annepro2/c18/keymaps/macvim/keymap.c
+++ keyboards/annepro2/c18/keymaps/macvim/keymap.c
@@ -120,4 +120,10 @@ void keyboard_post_init_user(void) {
   //debug_keyboard=true;
   //debug_mouse=true;
 }
+
+void reset_to_iap(void) {
+    //this will write eeprom make keyboard boot into iap mode
+    bootloader_jump();
+}
+
 // clang-format on
