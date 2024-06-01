27395a66c78a6356e2b92def39bdb4a40a8323b4
hitsmaxft <mfthits@gmail.com>
2024-06-01 14:23:16 +0800

    Cleanup: Remove unnecessary dependencies

--- keyboards/gh61ble/keymaps/macvim/rules.mk
+++ keyboards/gh61ble/keymaps/macvim/rules.mk
@@ -3,12 +3,6 @@ LEADER_ENABLE = yes
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes # Mouse keys
-EXTRAKEY_ENABLE = no # Audio control and System control
-NKRO_ENABLE = no            # Enable N-Key Rollover
-
-OS_DETECTION_ENABLE = yes
 
 # Custom RGB matrix handling
 RGB_MATRIX_ENABLE = yes
@@ -17,10 +11,6 @@ QMK_SETTINGS = yes
 TAP_DANCE_ENABLE = no
 COMBO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
-
-# home mod tapping
-CONSOLE_ENABLE = yes # Console for debug
-COMMAND_ENABLE = yes # Commands for debug and configuration
 
 
 SRC += os_autoconfig.c
