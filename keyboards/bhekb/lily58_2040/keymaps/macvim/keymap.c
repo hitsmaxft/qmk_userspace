fcf1b66b435d77dc4eb20b3199e9ad1407ca831a
hitsmaxft <mfthits@gmail.com>
2025-02-20 14:30:53 +0800

    update km

--- keyboards/bhekb/lily58_2040/keymaps/macvim/keymap.c
+++ keyboards/bhekb/lily58_2040/keymaps/macvim/keymap.c
@@ -253,12 +253,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12 , _______, _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_QUES, TRS_GRV ,
                              _______, _______, MO(LFUNC), _______, _______, MO(LFUNC), KC_NO  , KC_NO
 ),
+//adjust layer
 [LFUNC] = LAYOUT(
-  KC_F   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
-  KC_NO  , KC_NO  , KC_NO  , KC_MS_U, KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
-  KC_NO  , KC_MS_L, KC_MS_L, DB_TOGG, KC_MS_R, KC_F6  ,                   KC_LEFT, KC_DOWN, KC_UP  , KC_DOWN, KC_SCLN, KC_QUOT,
-  KC_NO  , KC_Z   , KC_X   , KC_C   , KC_HOME, QK_BOOT, KC_LBRC, KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                             KC_LALT, KC_LGUI, _______, KC_SPC , KC_ENT , KC_NO  , KC_NO  , KC_NO
+  KC_F   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , DB_TOGG,                   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , QK_BOOT,QK_REBOOT,DB_TOGG, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+                             KC_LALT, KC_LGUI, _______, KC_NO   , KC_ENT, KC_NO  , KC_NO  , KC_NO
 ),
 [LNAVI] = LAYOUT(
   KC_NO   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
