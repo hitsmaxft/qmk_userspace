9515dd0245ae072a522687a25858775bfa92c629
麒翔 <qixiang.mft@taobao.com>
2025-05-20 14:39:44 +0800

    update

--- keyboards/bhekb/lily58_2040/keymaps/macvim/keymap.c
+++ keyboards/bhekb/lily58_2040/keymaps/macvim/keymap.c
@@ -274,10 +274,10 @@ void matrix_scan_user(void) { // The very important timer.
 //chordal hold layout
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     LAYOUT(
-        'L', 'L', 'L', 'L', 'L', 'L',        'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',        'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',        'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L','*','*','R', 'R', 'R', 'R', 'R', 'R',
+        '*', '*', '*', '*', '*', '*',                 '*', '*', '*', '*', '*', '*',
+        '*', '*', 'L', 'L', 'L', '*',                 '*', 'R', 'R', 'R', '*', '*',
+        '*', 'L', 'L', 'L', 'L', 'L',                 'R', 'R', 'R', 'R', 'R', '*',
+        '*', '*', '*', '*', 'L', '*','*', '*','*', 'R', 'R', '*', '*', '*',
                        '*', '*', '*','*','*','*', '*', '*'
     );
 
@@ -311,7 +311,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 // FN line and quick symbol
-[LRAISE] = LAYOUT(
+[LRAISE] = LAYOUT_HRM(
   QK_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0    , KC_BSPC,
   KC_NO  , KC_F1   , KC_F2 , KC_F3  , KC_F4  , KC_F5  ,                        KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  , KC_F11  ,
   KC_NO  , KC_5   , KC_4   , KC_3   , KC_2   , KC_1   ,                                        KC_BSPC, KC_MINS, KC_EQL , KC_PLUS, KC_DQUO , KC_F12  ,
