4d76b9d7045915272be9bbf1005533ab2e407c5f
hitsmaxft <mfthits@gmail.com>
2025-06-04 15:34:22 +0800

    update keymap

--- keyboards/bhekb/lily58_2040/keymaps/macvim/config.h
+++ keyboards/bhekb/lily58_2040/keymaps/macvim/config.h
@@ -22,7 +22,7 @@
 #pragma once
 #define OLED_IC OLED_IC_SSD1306
 
-#define SPLIT_USB_DETECT
+//#define SPLIT_USB_DETECT
 #define OLED_CS_PIN 29
 #define OLED_DC_PIN 28
 #define OLED_DISPLAY_128X32 TRUE
--- keyboards/bhekb/lily58_2040/keymaps/macvim/keymap.c
+++ keyboards/bhekb/lily58_2040/keymaps/macvim/keymap.c
@@ -116,6 +116,10 @@ bool is_oppsite_hand_tap_flow(keyrecord_t *last, keyrecord_t *current) {
     return (last->event.key.col < border_for_splithand && current->event.key.col > border_for_splithand) || (last->event.key.col > border_for_splithand && current->event.key.col < border_for_splithand);
 }
 
+// This globally defines all key overrides to be used
+// const key_override_t  delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_GRAVE, KC_);
+// const key_override_t *key_overrides[]     = {&delete_key_override};
+
 // disable tap flow while pressing with two hand
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
     if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
@@ -127,7 +131,6 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_
         }
         // save last pressed key for next checking
         last_tap_flow_key_record = record;
-
         return TAP_FLOW_TERM;
     }
     return 0;
@@ -306,10 +309,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE_MAC] = LAYOUT_HRM(
 //FORMAT__START
-QK_GESC ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                        KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_BSPC ,
+
+QK_GESC ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                        KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_GRAVE,
 KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,                        KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
 QK_GESC ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                        KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
-KC_NO   ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,UK_VGCP ,UK_CAPR ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_GRV  ,
+LN_TAB  ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,UK_VGCP ,UK_CAPR ,     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_BSPC,
                                     KC_LSFT ,LN_TAB  ,TL_LOWR ,UK_SPC  ,KC_ENT  ,TL_UPPR ,KC_BSPC ,KC_RSFT
 //FORMAT__END
 //
@@ -340,8 +344,8 @@ KC_END  ,KC_TRNS ,KC_RBRC ,KC_NO   ,KC_PIPE ,KC_BSLS ,KC_NO   ,KC_NO   ,KC_SLSH 
 //FORMAT__START
 QK_GESC ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                        KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_BSPC ,
 KC_NO   ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                        KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,
-KC_NO   ,KC_5    ,KC_4    ,KC_3    ,KC_2    ,KC_1    ,                        KC_BSPC ,KC_MINS ,KC_EQL  ,KC_PLUS ,KC_DQUO ,KC_F12  ,
-KC_CAPS ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,_______ ,KC_BSPC ,KC_INS  ,KC_HOME ,KC_END  ,KC_QUES ,TRS_GRV ,
+KC_NO   ,KC_5    ,KC_4    ,KC_3    ,KC_2    ,KC_1    ,                                        KC_BSPC ,KC_MINS ,KC_EQL  ,KC_MINS ,KC_EQL  ,KC_F12  ,
+KC_CAPS ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,_______ ,KC_TAB  ,KC_INS  ,KC_HOME ,KC_END  ,KC_QUES ,TRS_GRV ,
                                     _______ ,_______ ,MO(LFUNC),_______ ,KC_DEL  ,KC_NO   ,KC_BSPC ,KC_NO
 //FORMAT__END
 ),
