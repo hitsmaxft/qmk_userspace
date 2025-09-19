e76c743fa52534ed9c85eab285b994eb06ac8fa1
hitsmaxft <mfthits@gmail.com>
2025-09-19 16:23:26 +0800

    update lily58 keymap

--- keyboards/lily58_2040/keymaps/macvim/keymap.c
+++ keyboards/lily58_2040/keymaps/macvim/keymap.c
@@ -28,6 +28,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
 #include "custom_keycodes.h"
@@ -58,7 +59,7 @@ QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_
 KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 QK_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 TAP_HOLD_NUM_TAB, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    CUSTOM_VIM_YANK,        CUSTOM_CAPTURE_REGION,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
-                                    KC_LSFT, TAP_HOLD_NUM_ESC, TL_LOWR, TAP_HOLD_SPACE, KC_ENT,  TL_UPPR, KC_BSPC, KC_RSFT
+                        KC_LSFT, TAP_HOLD_NUM_ESC, TL_LOWR, KC_SPACE, TAP_HOLD_ENT,  TL_UPPR, KC_BSPC, KC_RSFT
 //FORMAT__END
 //
 ),
